d4c7dc2ecc601ab64a8a2edb6e209b1cf09f9848
Jesse <jessekearl@gmail.com>
2024-08-07 15:57:20 -0600

    Added telescope live_grep_args

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,17 +1,18 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    require("nvim-tree").setup(
-      {
-        view = {
-          relativenumber = true
-        },
-        actions = {
-          open_file = {
-            quit_on_open = true,
-          },
-        },
-      }
-    )
-  end
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			view = {
+				relativenumber = true,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+			diagnostics = {
+				enable = true,
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,26 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{
+			"nvim-telescope/telescope-live-grep-args.nvim",
+			version = "^1.0.0",
+		},
+	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		telescope.setup({
 			pickers = {
 				buffers = {
 					sort_mru = true,
 				},
 			},
 		})
+		telescope.load_extension("live_grep_args")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 	end,
