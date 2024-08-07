b16969abe3f7817c193d5929d9b4ae38e07a9dff
Jesse <jessekearl@gmail.com>
2024-08-07 13:23:44 -0600

    nvm I like which-key globals

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -10,7 +10,8 @@ return {
 		{
 			"<leader>?",
 			function()
-				require("which-key").show({ global = false })
+				require("which-key").show()
+				-- require("which-key").show({ global = false })
 			end,
 			desc = "Buffer Local Keymaps (which-key)",
 		},
