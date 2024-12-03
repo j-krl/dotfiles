9a8770ebe341350883d31372f9a02d0c6e447d32
Jesse <jessekearl@gmail.com>
2024-12-02 17:58:43 -0700

    weird luasnip build stuff

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,7 +4,7 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = { "rafamadriz/friendly-snippets" },
-		build = "make install_jsregexp",
+		-- build = "make install_jsregexp",
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
 			luasnip = require("luasnip")
