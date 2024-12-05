3b24f2a82a4d9cab44b146c3e64568cad19a7107
Jesse <jessekearl@gmail.com>
2024-12-05 13:19:44 -0700

    last one

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
 require("config.lazy")
-
-if vim.g.vscode then
-    -- undo/REDO via vscode
-    vim.keymap.set("n","u","<Cmd>call VSCodeNotify('undo')<CR>")
-    vim.keymap.set("n","<C-r>","<Cmd>call VSCodeNotify('redo')<CR>") 
-end
