6227ceb24c9aa30ae9c7c360e1e1d828ab916740
Jesse Kearl <jessekearl@gmail.com>
2025-09-17 21:17:53 -0600

    revert my session file changes

--- tmux-session.sh
+++ tmux-session.sh
@@ -7,28 +7,27 @@ if [ $? != 0 ]; then
     tmux new-session -s $SES -d -x- -y-
     tmux set-option base-index 1
     tmux rename-window -t 1 "proj"
+    tmux send-keys "cd programming" C-m
     tmux split-window
+    tmux send-keys "cd programming" C-m
+    tmux resize-pane -y 15
     tmux select-pane -t "{top}"
-    tmux split-window -h
-    tmux resize-pane -y 12
-    tmux select-pane -t "{bottom}"
-    tmux split-window
-    tmux split-window -h
-    tmux resize-pane -y 12
-    tmux select-pane -U
     tmux new-window -t 2
-    tmux rename-window -t 2 "nb"
+    tmux send-keys "cd programming" C-m
+    tmux rename-window -t 2 "proj2"
+    tmux split-window
+    tmux send-keys "cd programming" C-m
+    tmux resize-pane -y 15
+    tmux select-pane -t "{top}"
+    tmux new-window -t 3
+    tmux rename-window -t 3 "nb"
     tmux split-window -h
-    tmux select-pane -L
-    tmux new-window -t 8
-    tmux rename-window -t 8 "srv"
-    tmux split-window -h
-    tmux select-pane -L
     tmux new-window -t 9
-    tmux rename-window -t 9 "term"
+    tmux rename-window -t 9 "cfg"
+    tmux send-keys "nvcd" C-m
     tmux split-window
-    tmux split-window
-    tmux select-layout even-vertical
+    tmux send-keys "nvcd" C-m
+    tmux resize-pane -y 15
     tmux select-pane -t "{top}"
     tmux select-window -t 1
 fi
